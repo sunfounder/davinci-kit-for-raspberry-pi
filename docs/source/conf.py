368fa0882322a7493ddc52b76b41e2cf5728ff63
zhuang <zhuang201704@gmail.com>
2023-10-31 10:02:50 +0800

    Add .readthedocs.yaml file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_js_files = [
     'https://ezblock.cc/readDocFile/custom.js',
+    './lang.js', # new
     'https://ezblock.cc/readDocFile/readTheDoc/src/js/ace.js',
     'https://ezblock.cc/readDocFile/readTheDoc/src/js/ext-language_tools.js',
     'https://ezblock.cc/readDocFile/readTheDoc/src/js/theme-chrome.js',
